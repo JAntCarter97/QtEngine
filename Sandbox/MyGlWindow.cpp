811b1985c373611aa0e150c5918581756bd111c3
John Carter <metalofthe3rdkind@yahoo.com>
2017-02-25 12:29:14 -0700

    Used moc.exe for MyGlWindow to generate proper moc files.

--- Sandbox/MyGlWindow.cpp
+++ Sandbox/MyGlWindow.cpp
@@ -11,10 +11,13 @@ void MyGlWindow::initializeGL()
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 
 	float verts[] = { 0.0f,  0.1f,
-					 -0.1f, -0.1f,
-					  0.1f, -0.1f };
+		-0.1f, -0.1f,
+		0.1f, -0.1f };
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	connect(&myTimer, SIGNAL(timeout()), this, SLOT(myUpdate()));
+	myTimer.start(0);
 }
 
 void MyGlWindow::paintGL()
@@ -23,4 +26,9 @@ void MyGlWindow::paintGL()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void MyGlWindow::myUpdate()
+{
+
 }
--- Sandbox/MyGlWindow.h
+++ Sandbox/MyGlWindow.h
@@ -1,10 +1,17 @@
 #pragma once 
 #include <qopenglwidget.h>
+#include <qtimer.h>
 
 class MyGlWindow : public QOpenGLWidget
 {
+	Q_OBJECT
+
 	GLuint vertexBufferID;
+	QTimer myTimer;
 protected:
 	void initializeGL();
 	void paintGL();
+
+private slots:
+	void myUpdate();
 };
