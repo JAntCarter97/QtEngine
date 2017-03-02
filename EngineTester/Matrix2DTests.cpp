8481b47895cc9266fb0a23f6ed7b8d75302e0ed6
John Carter <metalofthe3rdkind@yahoo.com>
2017-03-01 20:41:37 -0700

    Added Matrix2D and Rotations (All Tested in gtest)

---
+++ EngineTester/Matrix2DTests.cpp
@@ -0,0 +1,75 @@
+#include <Math/Matrix2D.h>
+#include <Math/Vector2D.h>
+#include <gtest/gtest.h>
+#include "HelperFunctions.h"
+
+using Math::Matrix2D;
+using Math::Vector2D;
+
+TEST(Matrix2D, Construction)
+{
+	Matrix2D identity;
+	EXPECT_FLOAT_EQ(identity.r0c0, 1.0f);
+	EXPECT_FLOAT_EQ(identity.r0c1, 0.0f);
+	EXPECT_FLOAT_EQ(identity.r1c0, 0.0f);
+	EXPECT_FLOAT_EQ(identity.r1c1, 1.0f);
+}
+
+TEST(Matrix2D, Rotation)
+{
+	const float PI = 3.14159265358979323846;
+	Matrix2D op;
+	op = Matrix2D::rotate(0);
+	EXPECT_FLOAT_EQ(op.r0c0, 1);
+	EXPECT_FLOAT_EQ(op.r0c1, 0);
+	EXPECT_FLOAT_EQ(op.r1c0, 0);
+	EXPECT_FLOAT_EQ(op.r1c1, 1);
+
+	op = Matrix2D::rotate(PI);
+	EXPECT_FLOAT_EQ(op.r0c0, -1);
+	EXPECT_TRUE(closeEnough(op.r0c1, 0));
+	EXPECT_TRUE(closeEnough(op.r1c0, 0));
+	EXPECT_FLOAT_EQ(op.r1c1, -1);
+
+	op = Matrix2D::rotate(PI / 2);
+	EXPECT_TRUE(closeEnough(op.r0c0, 0));
+	EXPECT_FLOAT_EQ(op.r0c1, -1);
+	EXPECT_FLOAT_EQ(op.r1c0, 1);
+	EXPECT_TRUE(closeEnough(op.r1c1, 0));
+
+	op = Matrix2D::rotate(PI / 4);
+	const float sqrt2Over2 = 0.707106769;
+	EXPECT_FLOAT_EQ(op.r0c0, sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r0c1, -sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r1c0, sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r1c1, sqrt2Over2);
+
+	op = Matrix2D::rotate(-PI / 4);
+	EXPECT_FLOAT_EQ(op.r0c0, sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r0c1, sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r1c0, -sqrt2Over2);
+	EXPECT_FLOAT_EQ(op.r1c1, sqrt2Over2);
+
+	op = Matrix2D::rotate(PI / 3);
+	const float sqrt3Over2 = 0.866025388;
+	EXPECT_FLOAT_EQ(op.r0c0, 0.5);
+	EXPECT_FLOAT_EQ(op.r0c1, -sqrt3Over2);
+	EXPECT_FLOAT_EQ(op.r1c0, sqrt3Over2);
+	EXPECT_FLOAT_EQ(op.r1c1, 0.5);
+
+	op = Matrix2D::rotate(-PI / 3);
+	EXPECT_FLOAT_EQ(op.r0c0, 0.5);
+	EXPECT_FLOAT_EQ(op.r0c1, sqrt3Over2);
+	EXPECT_FLOAT_EQ(op.r1c0, -sqrt3Over2);
+	EXPECT_FLOAT_EQ(op.r1c1, 0.5);
+
+}
+
+TEST(Matrix2D, MatrixVectorMultiply)
+{
+	Matrix2D op(2, -3, 4, -5);
+	Vector2D v(3, 9);
+	Vector2D vprime = op * v;
+	EXPECT_FLOAT_EQ(vprime.x, -21);
+	EXPECT_FLOAT_EQ(vprime.y, -33);
+}
--- QtEngine/Math/Matrix2D.h
+++ QtEngine/Math/Matrix2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector2D.h"
+#include <cmath>
 
 namespace Math
 {
@@ -12,8 +13,20 @@ namespace Math
 
 		// Default as Identity matrix
 		inline explicit Matrix2D(float r0c0 = 1.0f, float r0c1 = 0.0f,
-						  float r1c0 = 0.0f, float r1c1 = 1.0f) {}
+								 float r1c0 = 0.0f, float r1c1 = 1.0f) 
+								 : r0c0(r0c0), r0c1(r0c1), 
+								   r1c0(r1c0), r1c1(r1c1) {}
+		inline static Matrix2D rotate(float angleInRads)
+		{ 
+			return Matrix2D(cos(angleInRads), -sin(angleInRads), sin(angleInRads), cos(angleInRads)); 
+		}
 	};
 
-	inline Vector2D operator*(const Matrix2D& left, const Vector2D& right) {}
+	inline Vector2D operator*(const Matrix2D& mat, const Vector2D& vec)
+	{
+		Vector2D t;
+		t.x = (mat.r0c0 * vec.x) + (mat.r0c1 * vec.y);
+		t.y = (mat.r1c0 * vec.x) + (mat.r1c1 * vec.y);
+		return t;
+	}
 }
